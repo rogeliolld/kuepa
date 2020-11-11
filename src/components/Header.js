3c3ae762eaa7ed40521874885a35e7255f27cdea
rogeliolld <46878977+rogeliolld@users.noreply.github.com>
2020-11-10 21:15:49 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,28 +3,30 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <div class="col-md-4">
-          <div class="input-group">
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <div className="col-md-4">
+          <div className="input-group">
+            <form>
             <input
-              class="form-control py-2 border-right-0 border"
-              type="search"
-              value="Buscar"
-              id="example-search-input"
+              className="form-control py-2 border-right-0 border"
+              type="text" 
+              
+              defaultValue="Search..."
             />
-            <span class="input-group-append">
-              <div class="input-group-text bg-transparent">
-                <i class="fa fa-search"></i>
+           </form>  
+            <span className="input-group-append">
+              <div className="input-group-text bg-transparent">
+                <i className="fa fa-search"></i>
               </div>
             </span>
-            <span class="input-group-text icon-filtre">
-              <i class="fas fa-filter"></i>
+            <span className="input-group-text icon-filtre">
+              <i className="fas fa-filter"></i>
             </span>
           </div>
         </div>
-        <div class="btn-toolbar mb-2 mb-md-0 col-md-4">
-          <div class="btn-group mr-2">
-            <select class="form-control">
+        <div className="btn-toolbar mb-2 mb-md-0 col-md-4">
+          <div className="btn-group mr-2">
+            <select className="form-control">
               <option>Martes, 10 Noviembre</option>
             </select>
           </div>
